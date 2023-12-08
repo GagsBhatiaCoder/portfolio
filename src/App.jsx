8e13196d1101a6cef4d0f03727753a737e37e234
Gagan Bhatia <gagstaekwondo@gmail.com>
2023-12-08 21:13:47 +0530

    fixed loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Home from './Pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -8,11 +8,24 @@ import ContactForm from './Components/ContactForm';
 import BlogPage from './Blog/BlogPage';
 import AboutMePage from './Components/AboutMePage';
 import OurServicePage from './OurServices/OurServicePage';
+import { FadeLoader } from 'react-spinners';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  window.onload = () => {
+   setLoading(false)
+  }
 
   return (
     <>
+    { loading? <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh"}}><FadeLoader
+      color={"#000"}
+      loading={loading}
+      size={100}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    /> </div>:
      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -24,8 +37,10 @@ function App() {
           <Route path="/blog/:id" element={<FullArtical />} />
         </Routes>
       </Router>
+      }
     </>
   )
-}
+    }
+
 
 export default App
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,32 +6,18 @@ import OurServices from '../OurServices/OurServices'
 import AboutMe from '../Components/AboutMe'
 import Blog from '../Blog/Blog'
 import FloatWatsappBtn from '../Components/FloatWatsappBtn'
-import ClipLoader from "react-spinners/ClipLoader";
-import { useState } from "react";
+
 
 
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
 
- window.onload = () => {
-  setLoading(false)
- }
+  // useEffect(() => {
+  //   setLoading(false)
+  // }, [])
   return (
     <div>
-      { loading? 
-      <div style={{display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh"}}>
-      <ClipLoader
-      color={"#000"}
-      loading={loading}
-      size={100}
-      aria-label="Loading Spinner"
-      data-testid="loader"
-    /> </div> :
-    <div>
+    < div >
       <Navbar />
       <FloatWatsappBtn />
       <MainSection />
@@ -39,10 +25,7 @@ export default function Home() {
       <OurServices />
       <Blog />
       <Footer />
-      </div>
-
-      }
-      
-    </div>
+      </div> 
+    </div >
   )
 }
